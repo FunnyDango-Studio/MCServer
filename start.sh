ebf980bfb750a246e5edf272ae1e1695f6be3922
jason920612 <30766214+jason920612@users.noreply.github.com>
2020-09-11 19:31:07 +0800

    change jarname

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 echo echo "java 11"
-java -Xms6553M -Xmx8G -Dfile.encoding=UTF-8 -XX:+UnlockExperimentalVMOptions -XX:+UseZGC -jar yatopia-1.16.2-paperclip.jar nogui
+java -Xms6553M -Xmx8G -Dfile.encoding=UTF-8 -XX:+UnlockExperimentalVMOptions -XX:+UseZGC -jar yatopia-1.16.3-paperclip.jar nogui
 
 ./start.sh
