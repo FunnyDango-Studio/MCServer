3ed5980f23a168dc99f37780d6710548bdea7a21
jason920612 <30766214+jason920612@users.noreply.github.com>
2020-10-05 10:01:19 +0800

    設定自動更新版本
    不用再重google drive載了

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-
-echo echo "java 11"
-java -Xms6553M -Xmx8G -Dfile.encoding=UTF-8 -XX:+UnlockExperimentalVMOptions -XX:+UseZGC -jar 1.16.3.jar nogui
+rm yatopia.jar
+curl -O yatopia.jar https://api.yatopia.net/v2/latestBuild/download?branch=ver/1.16.3
+java -Xms6553M -Xmx8G -Dfile.encoding=UTF-8 -XX:+UnlockExperimentalVMOptions -XX:+UseZGC -jar yatopia.jar nogui
 
 ./start.sh
