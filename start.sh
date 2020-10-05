eeb5374c61066634f7ae4040fc4cd9596e1f2219
jason920612 <30766214+jason920612@users.noreply.github.com>
2020-10-05 21:53:03 +0800

    Update start.sh
    自動下載穩定版本

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 rm yatopia.jar
-curl -O yatopia.jar https://api.yatopia.net/v2/latestBuild/download?branch=ver/1.16.3
+curl -O yatopia.jar https://yatopia.net/downloadStable
 java -Xms6553M -Xmx8G -Dfile.encoding=UTF-8 -XX:+UnlockExperimentalVMOptions -XX:+UseZGC -jar yatopia.jar nogui
 
 ./start.sh
