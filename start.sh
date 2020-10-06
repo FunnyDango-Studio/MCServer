ddaddb9b5d0f0275e44de83f38a95c165281142a
jason920612 <30766214+jason920612@users.noreply.github.com>
2020-10-06 12:37:48 +0800

    fix command

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 rm yatopia.jar
-curl -O yatopia.jar https://yatopia.net/downloadStable
+curl -o yatopia.jar https://yatopia.net/downloadStable
 java -Xms6553M -Xmx8G -Dfile.encoding=UTF-8 -XX:+UnlockExperimentalVMOptions -XX:+UseZGC -jar yatopia.jar nogui
 
 ./start.sh
