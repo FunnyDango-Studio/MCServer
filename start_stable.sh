3551a5d8b20bcd648459726dc7afbf898297ab1f
jason920612 <jasonya2206@gmail.com>
2020-10-06 21:56:25 +0800

    設定自動更新核心(有最新穩定版和最新版)

--- start_stable.sh
+++ start_stable.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 rm yatopia.jar
-wget -O yatopia.jar https://api.yatopia.net/v2/latestBuild/download?branch=ver/1.16.3 yatopia.jar
+wget -O yatopia.jar https://yatopia.net/downloadStable yatopia.jar
 
 java -Xms6553M -Xmx8G -Dfile.encoding=UTF-8 -XX:+UnlockExperimentalVMOptions -XX:+UseZGC -jar yatopia.jar nogui
 
