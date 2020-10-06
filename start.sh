9378baf68c43e0c39913e166c943b477f7ac6aa2
jason920612 <30766214+jason920612@users.noreply.github.com>
2020-10-06 18:55:39 +0800

    好我放棄....

--- start.sh
+++ start.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-rm yatopia.jar
-curl -o yatopia.jar https://api.yatopia.net/v2/build/104/download?branch=ver/1.16.3
 java -Xms6553M -Xmx8G -Dfile.encoding=UTF-8 -XX:+UnlockExperimentalVMOptions -XX:+UseZGC -jar yatopia.jar nogui
 
 ./start.sh
