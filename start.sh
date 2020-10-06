19388361d88281201f1f83988f3b9161e93b112f
jason920612 <30766214+jason920612@users.noreply.github.com>
2020-10-06 18:53:48 +0800

    maybe fix????

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 rm yatopia.jar
 https://api.yatopia.net/v2/build/104/download?branch=ver/1.16.3
-java -Xms6553M -Xmx8G -Dfile.encoding=UTF-8 -XX:+UnlockExperimentalVMOptions -XX:+UseZGC -jar yatopia.jar nogui
+java -Xms6553M -Xmx8G -Dfile.encoding=UTF-8 -XX:+UnlockExperimentalVMOptions -XX:+UseZGC -jar yatopia-1.16.3-paperclip-b104.jar nogui
 
 ./start.sh
