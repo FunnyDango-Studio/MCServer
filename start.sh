3aad53c9024d1f576f564d1b0b273918751a0e7a
jason <jasonya2206@gmail.com>
2020-11-07 09:34:22 +0000

    update 1.16.4

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 
-java -Xms15G -Xmx15G -Dfile.encoding=UTF-8 -XX:+UseZGC -jar yatopia.jar nogui
+java -Xms15G -Xmx15G -Dfile.encoding=UTF-8 -jar yatopia.jar nogui
 
 ./start.sh
