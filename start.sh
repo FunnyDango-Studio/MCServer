4f57ca516f078b9230a134dc823cb1a708a0f248
jason <jasonya2206@gmail.com>
2020-11-07 09:30:11 +0000

    update

--- start.sh
+++ start.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-rm yatopia.jar
-wget -O yatopia.jar https://api.yatopia.net/v2/latestBuild/download?branch=ver/1.16.3 yatopia.jar
 
-java -Xms6553M -Xmx8G -Dfile.encoding=UTF-8 -XX:+UnlockExperimentalVMOptions -XX:+UseZGC -jar yatopia.jar nogui
+
+java -Xms15G -Xmx15G -Dfile.encoding=UTF-8 -XX:+UseZGC -jar yatopia.jar nogui
 
 ./start.sh
